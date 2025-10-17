e34ebd725775d16efdcdd50c00422b9467c83359
Lukaszvdblink <9025326@student.zadkine.nl>
2025-10-17 13:14:37 +0200

    Opdracht 3: Welkom

--- opdracht-02-useEffect/src/App.jsx
+++ opdracht-02-useEffect/src/App.jsx
@@ -1,14 +1,18 @@
+
+import React from "react";
+import WelcomeMessage from "./components/WelcomeMessage";
 import './App.css'
-import Clock from './components/Clock'
-
+import Clock from './Components/Clock'
+ 
 function App() {
-
+ 
   return (
     <>
       <h1>Opdracht 2 - UseEffect</h1>
-      <Clock/>
+      <Clock />
+      <WelcomeMessage/>
     </>
   )
 }
-
+ 
 export default App
