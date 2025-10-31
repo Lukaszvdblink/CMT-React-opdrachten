e862f5c5fdd0277a2a959340eadc3bdd0111416e
Lukaszvdblink <9025326@student.zadkine.nl>
2025-10-31 12:18:25 +0100

    opdracht 6 zoo-app

---
+++ opdracht-06-zoo-app/src/pages/HomePage.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import animals from '../animals.js';
+import Card from '../components/Card.jsx';
+
+const Home = () => {
+  const navigate = useNavigate();
+  const [animalList, setAnimalList] = useState(animals);
+
+  const goToDetail = (animalId) => {
+    navigate(`/animals/${animalId}`);
+  };
+
+  return (
+    <section>
+      <h1>Welkom bij mijn website</h1>
+      <h2>Onze Dieren</h2>
+      
+      {animalList.map(animal => (
+        <Card
+          key={animal.id}
+          animals={animal}
+          onShowDetail={goToDetail}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default Home;
