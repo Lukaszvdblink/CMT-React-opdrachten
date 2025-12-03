3e7f471062b9a09bbf3cb0f4dd3c8df481dc8a54
Lukaszvdblink <9025326@student.zadkine.nl>
2025-12-03 14:15:17 +0100

    opdracht 9

--- opdracht-09-tailwind/src/App.jsx
+++ opdracht-09-tailwind/src/App.jsx
@@ -4,14 +4,14 @@ function App() {
 
   return (
     <>
-      <section >
+      <section className='mx-auto w-1/2'>
         <section className='w-48 '>
-          <div>
-            <img src="" alt="" />
-            <h1>Workcation</h1>
+          <div className='flex'>
+            <img src="/img/foto2.png" alt="Workcation" />
+            <h1>Work<span className='text-violet-300'>cation</span></h1>
           </div>
-          <img src="" alt="" />
-          <h2>you can work form anywhere. <span>take advantage of it</span></h2>
+         <img className='w-64' src="/img/foto.png" alt="Workcation" />
+          <h2 className='text-2xl font-bold'>you can work form anywhere. <span className='text-violet-300'>take advantage of it</span></h2>
           <p>workation helps you find work-friendly rentals in beautiful locations so you can enjoy some nice weather even when your not on vacation</p>
 
           <button className='bg-violet-300'>Book your escape</button>
