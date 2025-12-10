f4eb421ddb58082a12cd6a648ca516385befd9e5
Lukaszvdblink <9025326@student.zadkine.nl>
2025-12-10 13:44:37 +0100

    opdracht 12 weather app

--- opdracht-12-weather-app/src/App.jsx
+++ opdracht-12-weather-app/src/App.jsx
@@ -1,12 +1,50 @@
 import './App.css'
+import { useState, useEffect } from 'react';
+import WeatherInfo from './components/WeatherInfo';
 
 function App() {
+ const [weather, setWeather] = useState();
+ const [city, setCity] = useState('oslo')
+ const [input, setInput] = useState('')
 
+ const apiKey = "653685a1a12b47f3cb252e46696f9f24"
+
+  useEffect(() => {
+    
+    const fetchWeather = async () => {
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+        );
+        const weatherData = await response.json();
+        console.log(weatherData)
+        setWeather(weatherData);
+      } catch (error) {
+        console.error('Error fetching weather:', error);
+      }
+    };
+
+    fetchWeather();
+  }, [city]); 
+  function inputHandler(e) {
+    e.preventDefault();
+    setCity(input);
+  }
   return (
-    <>
-      <h1>Opdracht 12 - Weather App</h1>
-    </>
-  )
-}
+    <div>
+      <input type="text" onChange={(e) => setInput(e.target.value)} />
+      <button onClick={inputHandler}>zoek</button>
+      {weather ? (
+       <WeatherInfo city={weather.name} temp={weather.main.temp} feels={weather.main.feels_like} humidity={weather.main.humidity} wind={weather.wind.speed}  />
+
+  ) : (
+         <p>Het weer is aan het laden</p>
+      )}
+    </div>
+
+    );
+  
+};
+
 
 export default App
--- opdracht-13-flashcard-quiz/src/App.jsx
+++ opdracht-13-flashcard-quiz/src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+ 
+  const [flipped, setFlipped] = useState(true)
+
+ const flippHandler = () => {
+  setFlipped(!flipped)
+ }
 
   return (
     <>
       <h1>Opdracht 13 - Flashcard Quiz</h1>
+       <section onClick={flippHandler}>
+        {flipped ? <h2>vraag</h2> : <h2>antwoord</h2> }
+       </section>
+       
+      
     </>
   )
 }
