5b40a83277df26a3933e490eb9f1f120b9be9230
Lukaszvdblink <9025326@student.zadkine.nl>
2025-12-16 10:15:20 +0100

    opdracht 13.1

--- opdracht-01-books/src/App.jsx
+++ opdracht-01-books/src/App.jsx
@@ -7,6 +7,7 @@ import Contact from './pages/Contact';
 import Navigation from './pages/Navigation';
 
 
+
 function App() {
 
   return (
--- opdracht-01-books/src/components/Book.jsx
+++ opdracht-01-books/src/components/Book.jsx
@@ -5,11 +5,21 @@ const Book = ({image, title, author}) => {
     function  verhoogTeller() {
         setAantalKeerGelezen(aantalKeerGelezen + 1)
     }
+    
+    const [liked, setLiked] = useState(false);
+    const toggleLike = () => {
+        setLiked(!liked)
+    }
+
+
     return (
  <section className="book-container">
 <img src={image} alt=""  className="image"/>
 <h2>{title}</h2>
 <h3>{author}</h3>
+<div>
+    <button onClick={toggleLike}>{liked ? <h1>❤️</h1> : <h1>🤍</h1>}</button>
+</div>
 <button onClick={verhoogTeller}>
 Keer gelezen: {aantalKeerGelezen}
 </button>
